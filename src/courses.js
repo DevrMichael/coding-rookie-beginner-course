fe8c869b4b9f77975ea37c2e596d0b489f3215f9
DevrMichael <88589247+DevrMichael@users.noreply.github.com>
2023-01-22 20:15:14 +0100

    Add HTML, CSS & JS Lessons
    - 15 lesson topics for each course was added

--- src/courses.js
+++ src/courses.js
@@ -12,7 +12,85 @@ const courses = [
             },
             {
                 id: 2,
-                title: "Welcome to the first lesson",
+                title: "What is HTML",
+                description: "Lorem ipsum", 
+                vimeoId: 76979871
+            },
+            {
+                id: 3,
+                title: "The Structure of HTML Pages",
+                description: "Lorem ipsum", 
+                vimeoId: 76979871
+            },
+            {
+                id: 4,
+                title: "HTML Tags & Attributes",
+                description: "Lorem ipsum", 
+                vimeoId: 76979871
+            },
+            {
+                id: 5,
+                title: "Headings & Paragraphs in HTML",
+                description: "Lorem ipsum", 
+                vimeoId: 76979871
+            },
+            {
+                id: 6,
+                title: "Text Formatting in HTML",
+                description: "Lorem ipsum", 
+                vimeoId: 76979871
+            },
+            {
+                id: 7,
+                title: "HTML Images",
+                description: "Lorem ipsum", 
+                vimeoId: 76979871
+            },
+            {
+                id: 8,
+                title: "Tables & Lists in HTML",
+                description: "Lorem ipsum", 
+                vimeoId: 76979871
+            },
+            {
+                id: 9,
+                title: "HTML Forms",
+                description: "Lorem ipsum", 
+                vimeoId: 76979871
+            },
+            {
+                id: 10,
+                title: "Creating a Simple HTML Page from Scratch",
+                description: "Lorem ipsum", 
+                vimeoId: 76979871
+            },
+            {
+                id: 11,
+                title: "Styling HTML Elements",
+                description: "Lorem ipsum", 
+                vimeoId: 76979871
+            },
+            {
+                id: 12,
+                title: "HTML Classes & IDs",
+                description: "Lorem ipsum", 
+                vimeoId: 76979871
+            },
+            {
+                id: 13,
+                title: "HTML Links with Anchor Tags",
+                description: "Lorem ipsum", 
+                vimeoId: 76979871
+            },
+            {
+                id: 14,
+                title: "Comments on HTML Pages",
+                description: "Lorem ipsum", 
+                vimeoId: 76979871
+            },
+            {
+                id: 15,
+                title: "HTML Cheatsheet",
                 description: "Lorem ipsum", 
                 vimeoId: 76979871
             },
@@ -32,7 +110,85 @@ const courses = [
         },
         {
             id: 2,
-            title: "Welcome to the first lesson",
+            title: "What is CSS?",
+            description: "Lorem ipsum", 
+            vimeoId: 76979871
+        },
+        {
+            id: 3,
+            title: "CSS Selectors",
+            description: "Lorem ipsum", 
+            vimeoId: 76979871
+        },
+        {
+            id: 4,
+            title: "Setting Colors with CSS",
+            description: "Lorem ipsum", 
+            vimeoId: 76979871
+        },
+        {
+            id: 5,
+            title: "The Box Model",
+            description: "Lorem ipsum", 
+            vimeoId: 76979871
+        },
+        {
+            id: 6,
+            title: "Padding & Margin in CSS",
+            description: "Lorem ipsum", 
+            vimeoId: 76979871
+        },
+        {
+            id: 7,
+            title: "Borders",
+            description: "Lorem ipsum", 
+            vimeoId: 76979871
+        },
+        {
+            id: 8,
+            title: "Height & Width in CSS",
+            description: "Lorem ipsum", 
+            vimeoId: 76979871
+        },
+        {
+            id: 9,
+            title: "The Display Property",
+            description: "Lorem ipsum", 
+            vimeoId: 76979871
+        },
+        {
+            id: 10,
+            title: "Styling Text Using CSS",
+            description: "Lorem ipsum", 
+            vimeoId: 76979871
+        },
+        {
+            id: 11,
+            title: "Customizing the Background with CSS",
+            description: "Lorem ipsum", 
+            vimeoId: 76979871
+        },
+        {
+            id: 12,
+            title: "Screen Customization & Media Queries",
+            description: "Lorem ipsum", 
+            vimeoId: 76979871
+        },
+        {
+            id: 13,
+            title: "The Z-Index",
+            description: "Lorem ipsum", 
+            vimeoId: 76979871
+        },
+        {
+            id: 14,
+            title: "CSS Align",
+            description: "Lorem ipsum", 
+            vimeoId: 76979871
+        },
+        {
+            id: 15,
+            title: "CSS Cheatsheet",
             description: "Lorem ipsum", 
             vimeoId: 76979871
         },
@@ -51,7 +207,85 @@ const courses = [
     },
     {
         id: 2,
-        title: "Welcome to the first lesson",
+        title: "What is Javascript?",
+        description: "Lorem ipsum", 
+        vimeoId: 76979871
+    },
+    {
+        id: 3,
+        title: "Javascript Values",
+        description: "Lorem ipsum", 
+        vimeoId: 76979871
+    },
+    {
+        id: 4,
+        title: "Creating Variables",
+        description: "Lorem ipsum", 
+        vimeoId: 76979871
+    },
+    {
+        id: 5,
+        title: "Operators in Javascript",
+        description: "Lorem ipsum", 
+        vimeoId: 76979871
+    },
+    {
+        id: 6,
+        title: "Statements in Javascript",
+        description: "Lorem ipsum", 
+        vimeoId: 76979871
+    },
+    {
+        id: 7,
+        title: "Events in Javascript",
+        description: "Lorem ipsum", 
+        vimeoId: 76979871
+    },
+    {
+        id: 8,
+        title: "Objects in Javascript",
+        description: "Lorem ipsum", 
+        vimeoId: 76979871
+    },
+    {
+        id: 9,
+        title: "Arrays in Javascript",
+        description: "Lorem ipsum", 
+        vimeoId: 76979871
+    },
+    {
+        id: 10,
+        title: "Functions in Javascript",
+        description: "Lorem ipsum", 
+        vimeoId: 76979871
+    },
+    {
+        id: 11,
+        title: "Methods in Javascript",
+        description: "Lorem ipsum", 
+        vimeoId: 76979871
+    },
+    {
+        id: 12,
+        title: "Loops in Javascript",
+        description: "Lorem ipsum", 
+        vimeoId: 76979871
+    },
+    {
+        id: 13,
+        title: "If Statements in Javascript",
+        description: "Lorem ipsum", 
+        vimeoId: 76979871
+    },
+    {
+        id: 14,
+        title: "Switch Statements in Javascript",
+        description: "Lorem ipsum", 
+        vimeoId: 76979871
+    },
+    {
+        id: 15,
+        title: "Javascript Cheatsheet",
         description: "Lorem ipsum", 
         vimeoId: 76979871
     },
