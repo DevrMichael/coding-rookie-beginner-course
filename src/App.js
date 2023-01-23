5db44da5186583bff7514497901c970c0b8cf6f8
DevrMichael <88589247+DevrMichael@users.noreply.github.com>
2023-01-23 17:20:23 +0100

    Import Firebase Authentication

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
+import { useState } from 'react'
 import Home from './pages/Home'
 import Lesson from './pages/Lesson'
 import Course from './pages/Course'
+import Login from './pages/Login'
+import { signOut } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
+import { auth } from './firebase-config'
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false)
+
+  let navigate = useNavigate()
+
+  const signUserOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear()
+      setIsAuth(false)
+      navigate("/login")
+    })
+  }
   return (
     <div className='App'>
       <main>
+        <nav>
+              {isAuth && <Link className='button primary' to="/">Home</Link>}
+              {isAuth && <Link className='button primary' to="/course">Course</Link>}
+              {!isAuth ? <Link className='button primary' to="/login">Login</Link> : <button className='button primary' onClick={signUserOut}>Log Out</button>}
+        </nav>
         <Routes>
-          <Route 
+          {isAuth && <Route 
             path="/"
             element={<Home />}
-          />
-          <Route 
+          />}
+          {isAuth && <Route 
             path="/courses/:courseId"
             element={<Course />}
-          />
-          <Route 
+          />}
+          {isAuth && <Route 
             path="/courses/:courseId/lessons/:lessonId"
             element={<Lesson />}
+          />}
+          <Route 
+            path="/login"
+            element={<Login setIsAuth={setIsAuth} />}
           />
         </Routes>
       </main>
