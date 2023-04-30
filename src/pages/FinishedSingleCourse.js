f5d5051cdc1d9c125b1b0afe47eb6251800dbc12
DevrMichael <88589247+DevrMichael@users.noreply.github.com>
2023-04-30 20:42:20 +0200

    Fix comparison sign

--- src/pages/FinishedSingleCourse.js
+++ src/pages/FinishedSingleCourse.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { Link, useParams } from "react-router-dom";
-import NextCourseButton from "../components/NextCourseButton";
-import courses from "../data/courses";
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import NextCourseButton from '../components/NextCourseButton';
+import courses from '../data/courses';
 
 function FinishedSingleCourse() {
   const { courseId } = useParams();
@@ -13,10 +13,13 @@ function FinishedSingleCourse() {
         <h1>Congratulations!</h1>
         <h2>You completed the {course.title} course!</h2>
         {courseId < 3 && <NextCourseButton courseId={courseId} />}
-        {courseId == 3 && (
+        {courseId === 3 && (
           <>
-          <p>The next step is to check out the most frequently asked questions and answers about becoming a frontend developer.</p>
-          <br />
+            <p>
+              The next step is to check out the most frequently asked questions
+              and answers about becoming a frontend developer.
+            </p>
+            <br />
             <Link className="button primary" to="/faq">
               Check out our FAQ
             </Link>
